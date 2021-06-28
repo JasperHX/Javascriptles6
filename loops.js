016f8990cea4e844c8fc8ec5525006c067dbffdd
JasperHX <jasper-hendrikx@hotmail.com>
2021-06-28 09:29:46 +0200

    loops

--- loops.js
+++ loops.js
@@ -6,7 +6,16 @@
 // Hoera!
 // ==========================================
 
-
+// stap 1 Hoera declareren
+// stap 2 for loop maken
+// stap 3 i is kleiner dan 3
+// console.log
+//
+// const celebrations = 'Hoera!';
+//
+// for (let i = 0; i < 3; i++) {
+//     console.log(celebrations)
+// };
 
 // ==========================================
 // Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de vijfde loop het woord "klaar!"
@@ -16,10 +25,23 @@
 // loop...
 // loop...
 // klaar!
- // ==========================================
+// ==========================================
 
-
-
+// stap 1 loop en klaar declareren
+// stap 2 for loop maken
+// stap 3 for loop met 5 items
+// stap 4 alles kleiner dan 4 is loop en al het andere is klaar (dus groter dan 5)
+//
+// const rounds = "loop...";
+// const ready = "klaar!";
+//
+// for (let i = 0; i < 5; i++) {
+//     if (i < 4) {
+//         console.log(rounds);
+//     } else {
+//         console.log(ready);
+//     }
+// }
 // ==========================================
 // Opdracht 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even of oneven is.
 // Verwachte uitkomsten:
@@ -30,10 +52,29 @@
 // .... etc.
 // 15 is oneven
 
+// stap 1 15 declareren
+// stap 2 for loop maken waarbij i < 16 is (totaal 15 nummers beginnend bij 0
+// stap 3 twee if statements
+
+// Oefening:
+//
+//     let fir = 15;
+// if (15 % 2 === 0) {
+//     console.log('bijna')
+// } else (console.log('niet goed'))
+
+// const numbers = 15;
+
+// for (let i = 0; i < 16; i++) {
+//     if (i % 2 === 0) {
+//         console.log('even');
+//     }
+//     if (i % 2 !== 0)
+//         console.log('oneven');
+// }
+
 // Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over..
 // ==========================================
-
-
 
 // ==========================================
 // [BONUSOPDRACHT] - optioneel: maak een for-loop die van 0 tot 9 loopt en de getallen 0 tot 9 logt.
@@ -51,3 +92,34 @@
 // >> 9
 // ==========================================
 
+// stap 1 declareer variabelen dus > en >>
+// stap 2 for loop maken die getallen 0 t/m 9 logt
+// stap 3 wanneer hoger dan 3 voeg je aan alles > toe
+// stap 4 wanneer hoger dan 6 komt er >> toe
+
+
+let from3 = '>'
+let from6 = '>>'
+
+for (let i = 0; i <= 9; i++) {
+    if (i >= 3) {
+        i += from3;
+        if (i >= 6)
+            i += from6;
+    }
+    console.log(i)
+}
+
+
+//     if (firstNumber >= 3) {
+//         console.log('>' + firstNumber)}
+//     if (firstNumber >= 6){
+//         console.log('>>' + firstNumber)}
+// }
+
+// console.log('>' +firstNumber)
+// } if (firstNumber >= 3) {
+//     console.log('>' + firstNumber)
+// } if (firstNumber >= 6) {
+//     console.log('>>' + firstNumber)
+// }
